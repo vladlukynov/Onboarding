b9ae9e7343ef8f0595a40689bedb7e232eda9e09
Андрей Усанов <iambreaddd@mail.ru>
2023-02-17 22:14:56 +0300

    Add all courses page

---
+++ client/app/src/main/java/com/src/onboarding/presentation/courses/all_courses/AllCoursesFragment.kt
@@ -0,0 +1,29 @@
+package com.src.onboarding.presentation.courses.all_courses
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.src.onboarding.R
+import com.src.onboarding.databinding.FragmentAllCoursesBinding
+
+class AllCoursesFragment : Fragment() {
+    lateinit var binding: FragmentAllCoursesBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAllCoursesBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
