cfe0dfdc2f2241738d8777af2c99eebe889d5ef7
vladlukynov <lukynov.1@yandex.ru>
2023-02-17 20:51:56 +0300

    Add course details fragment

---
+++ client/app/src/main/java/com/src/onboarding/presentation/courses/CourseDetailsFragment.kt
@@ -0,0 +1,26 @@
+package com.src.onboarding.presentation.courses
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.src.onboarding.databinding.FragmentCourseDetailsBinding
+
+class CourseDetailsFragment : Fragment() {
+    lateinit var binding: FragmentCourseDetailsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        binding = FragmentCourseDetailsBinding.inflate(inflater)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
