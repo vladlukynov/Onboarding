d6f33019281dbc7d66ca524bc3bc2b5b50cbd498
Андрей Усанов <iambreaddd@mail.ru>
2023-02-18 19:58:20 +0300

    Add transition in profile

--- client/app/src/main/java/com/src/onboarding/presentation/profile/UserProfileFragment.kt
+++ client/app/src/main/java/com/src/onboarding/presentation/profile/UserProfileFragment.kt
@@ -1,11 +1,14 @@
 package com.src.onboarding.presentation.profile
 
+import android.graphics.Color
+import android.opengl.Visibility
 import android.os.Binder
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.src.onboarding.R
 import com.src.onboarding.databinding.FragmentUserProfileBinding
 
@@ -27,6 +30,37 @@ class UserProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setBackButtonOnClick()
+
+        binding.tvAchievements.setOnClickListener {
+            binding.rvActivity.visibility = View.INVISIBLE
+            binding.rvStatistics.visibility = View.INVISIBLE
+            binding.ivMan.visibility = View.VISIBLE
+
+            binding.tvActivity.setTextColor(ContextCompat.getColor(requireContext(), R.color.text_grey))
+            binding.tvStatistics.setTextColor(ContextCompat.getColor(requireContext(), R.color.text_grey))
+            binding.tvAchievements.setTextColor(ContextCompat.getColor(requireContext(), R.color.main_purple))
+        }
+
+        binding.tvActivity.setOnClickListener {
+            binding.rvActivity.visibility = View.VISIBLE
+            binding.rvStatistics.visibility = View.INVISIBLE
+            binding.ivMan.visibility = View.INVISIBLE
+
+            binding.tvActivity.setTextColor(ContextCompat.getColor(requireContext(), R.color.main_purple))
+            binding.tvStatistics.setTextColor(ContextCompat.getColor(requireContext(), R.color.text_grey))
+            binding.tvAchievements.setTextColor(ContextCompat.getColor(requireContext(), R.color.text_grey))
+        }
+
+        binding.tvStatistics.setOnClickListener {
+            binding.rvActivity.visibility = View.INVISIBLE
+            binding.rvStatistics.visibility = View.VISIBLE
+            binding.ivMan.visibility = View.INVISIBLE
+
+            binding.tvActivity.setTextColor(ContextCompat.getColor(requireContext(), R.color.text_grey))
+            binding.tvStatistics.setTextColor(ContextCompat.getColor(requireContext(), R.color.main_purple))
+            binding.tvAchievements.setTextColor(ContextCompat.getColor(requireContext(), R.color.text_grey))
+        }
+
     }
 
     private fun setBackButtonOnClick() {
